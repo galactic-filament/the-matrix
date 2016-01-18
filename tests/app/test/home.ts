660c02b98e33af318f230b4790bfa4a13150ab69
Adrian Parker <iamhiddensomewhere@gmail.com>
2016-01-17 19:58:35 -0500

    Add post GET and DELETE endpoints. Update tsconfig.json to not compile on save.

--- tests/app/test/home.ts
+++ tests/app/test/home.ts
@@ -4,6 +4,22 @@ import chai = require("chai");
 
 let expect = chai.expect;
 let request = supertest("http://ApiServer");
+
+interface PostCallback {
+  (id: number): void;
+}
+let createPost = (cb: PostCallback) => {
+  request
+    .post("/posts")
+    .send({ body: "Hello, world!" })
+    .end((err: Error, res: supertest.Response) => {
+      expect(err).to.equal(null);
+      expect(res.status).to.equal(200);
+      expect(typeof res.body.id).to.equal("number");
+      cb(res.body.id);
+    });
+};
+
 describe("Homepage", () => {
     it("Should return standard greeting", (done: MochaDone) => {
       request
@@ -44,14 +60,32 @@ describe("Json reflection", () => {
 });
 describe("Post creation endpoint", () => {
   it("Should return the new post's id", (done: MochaDone) => {
-    request
-      .post("/posts")
-      .send({ body: "Hello, world!" })
-      .end((err: Error, res: supertest.Response) => {
-        expect(err).to.equal(null);
-        expect(res.status).to.equal(200);
-        expect(typeof res.body.id).to.equal("number");
-        done();
-      });
+    createPost((id: number) => {
+      done();
+    });
+  });
+});
+describe("Post endpoint", () => {
+  it("Should return a post", (done: MochaDone) => {
+    createPost((id: number) => {
+      request
+        .get("/post/" + id)
+        .end((err: Error, res: supertest.Response) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          done();
+        });
+    });
+  });
+  it("Should delete a post", (done: MochaDone) => {
+    createPost((id: number) => {
+      request
+        .delete("/post/" + id)
+        .end((err: Error, res: supertest.Response) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          done();
+        });
+    });
   });
 });
