050473ad7448a86814c0350f3478f2135a9df720
Adrian Parker <adrian.parker@smith.co>
2017-03-21 13:56:14 -0400

    Add test for creating new repo where the image already exists.

--- app/repo/repo_test.go
+++ app/repo/repo_test.go
@@ -38,8 +38,39 @@ func TestNonexistentNewRepo(t *testing.T) {
 	}
 
 	if hasImage {
+		t.Logf("Removing image %s to be pulled by newRepo()", imageID)
 		if err := client.RemoveImage(imageID); err != nil {
 			t.Errorf("Could not remove default repo image %s :%s", imageID, err.Error())
+			return
+		}
+	}
+
+	if _, err := newRepo(defaultRepoName, client); err != nil {
+		t.Errorf("Could not create new repo %s :%s", defaultRepoName, err.Error())
+		return
+	}
+}
+
+func TestExistentNewRepo(t *testing.T) {
+	dockerClient, err := docker.NewClientFromEnv()
+	if err != nil {
+		t.Errorf("Could not create a new docker client: %s", err.Error())
+		return
+	}
+	client := simpledocker.NewClient(dockerClient)
+
+	imageID := GetImageID(defaultRepoName)
+	hasImage, err := client.HasImage(imageID)
+	if err != nil {
+		t.Errorf("Could not validate whether default repo image exists %s: %s", imageID, err.Error())
+		return
+	}
+
+	if !hasImage {
+		t.Logf("Repo image %s was not found, pulling manually", imageID)
+		err = client.PullImage(imageID, defaultRepoImageTag)
+		if err != nil {
+			t.Errorf("Could not pull default repo image %s: %s", imageID, err.Error())
 			return
 		}
 	}
