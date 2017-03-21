808d5711bd5d3801ffddb8c7ac4ab51aba2fb112
Adrian Parker <adrian.parker@smith.co>
2017-03-21 14:01:52 -0400

    Add test for NewRepos().

---
+++ app/repo/repotask_test.go
@@ -0,0 +1,22 @@
+package repo
+
+import (
+	"testing"
+
+	docker "github.com/fsouza/go-dockerclient"
+	"github.com/ihsw/the-matrix/app/simpledocker"
+)
+
+func TestNewRepos(t *testing.T) {
+	dockerClient, err := docker.NewClientFromEnv()
+	if err != nil {
+		t.Errorf("Could not create a new docker client: %s", err.Error())
+		return
+	}
+	client := simpledocker.NewClient(dockerClient)
+
+	repoNames := []string{defaultRepoName}
+	if _, err := NewRepos(repoNames, client); err != nil {
+		t.Errorf("Could not create new repos from repo %s: %s", defaultRepoName, err.Error())
+	}
+}
