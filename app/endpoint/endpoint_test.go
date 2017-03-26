c99dd533daac80baa766118053b391a806f7160a
Adrian Parker <iamhiddensomewhere@gmail.com>
2017-03-25 21:51:47 -0400

    Start filling out the test for new-endpoint.

--- app/endpoint/endpoint_test.go
+++ app/endpoint/endpoint_test.go
@@ -1,7 +1,62 @@
 package endpoint
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	docker "github.com/fsouza/go-dockerclient"
+	"github.com/ihsw/the-matrix/app/repo"
+	"github.com/ihsw/the-matrix/app/resource"
+	"github.com/ihsw/the-matrix/app/simpledocker"
+)
+
+const defaultResourceName = "db"
+const defaultRepoName = "es-bueno"
 
 func TestNewEndpoint(t *testing.T) {
+	dockerClient, err := docker.NewClientFromEnv()
+	if err != nil {
+		t.Errorf("Could not create a new docker client: %s", err.Error())
+		return
+	}
+	client := simpledocker.NewClient(dockerClient)
 
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.Errorf("Could not get working dir: %s", err.Error())
+		return
+	}
+	resourceDir := fmt.Sprintf("%s/../../%s", cwd, defaultResourceName)
+
+	endpointResource, err := resource.NewResource(client, resource.Opts{
+		Name:                 defaultResourceName,
+		DockerfileContextDir: resourceDir,
+		EndpointEnvVars:      map[string]string{"DATABASE_HOST": "Db"},
+	})
+	if err != nil {
+		t.Errorf("Could not create a new resource with default resource %s: %s", defaultResourceName, err.Error())
+		return
+	}
+
+	repo, err := repo.NewRepo(defaultRepoName, client)
+	if err != nil {
+		t.Errorf("Could not create new repo %s: %s", defaultRepoName, err.Error())
+	}
+
+	endpoint, err := NewEndpoint(
+		repo,
+		resource.Resources{
+			Values: []resource.Resource{endpointResource},
+		},
+	)
+	if err != nil {
+		t.Errorf("Could not create a new endpoint based on repo %s: %s", repo.Name, err.Error())
+		return
+	}
+
+	if err := endpoint.Clean(); err != nil {
+		t.Errorf("Could not clean endpoint %s: %s", endpoint.Name, err.Error())
+		return
+	}
 }
