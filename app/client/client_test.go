8c53bd5471914b478309a414dd01f82a01552f51
Adrian Parker <adrian.parker@smith.co>
2017-04-18 16:30:34 -0400

    Uncomment out deferred test cleaning.

--- app/client/client_test.go
+++ app/client/client_test.go
@@ -26,7 +26,7 @@ func TestRun(t *testing.T) {
 		t.Errorf("Could not create network: %s", err.Error())
 		return
 	}
-	// defer simpledocker.CleanupNetwork(t, client, clientNetwork)
+	defer simpledocker.CleanupNetwork(t, client, clientNetwork)
 
 	// creating the endpoint resource
 	endpointResource, err := resource.CreateTestResource(resource.CreateTestResourceOpts{
@@ -40,7 +40,7 @@ func TestRun(t *testing.T) {
 		t.Errorf("Could not create resource: %s", err.Error())
 		return
 	}
-	// defer resource.CleanResource(t, endpointResource)
+	defer resource.CleanResource(t, endpointResource)
 
 	// creating the endpoint
 	clientEndpoint, err := endpoint.CreateTestEndpoint(endpoint.CreateTestEndpointOpts{
@@ -53,7 +53,7 @@ func TestRun(t *testing.T) {
 		t.Errorf("Could not create endpoint: %s", err.Error())
 		return
 	}
-	// defer endpoint.CleanEndpoint(t, clientEndpoint)
+	defer endpoint.CleanEndpoint(t, clientEndpoint)
 
 	// creating a client
 	clientRepo, err := repo.NewRepo(DefaultTestClientName, client)
@@ -69,7 +69,7 @@ func TestRun(t *testing.T) {
 		t.Errorf("Could not run endpoint against client: %s", err.Error())
 		return
 	}
-	// defer CleanClient(t, c, clientContainer)
+	defer CleanClient(t, c, clientContainer)
 
 	if err == ErrClientFailed {
 		containerOutput, err := c.Client.GetContainerLogs(clientContainer)
